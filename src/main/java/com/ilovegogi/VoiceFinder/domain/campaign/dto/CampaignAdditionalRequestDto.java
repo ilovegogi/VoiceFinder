72aa9dd54d25b17486dda76e2ad5fccfd2d779c5
sehwan24 <1224ssanta@naver.com>
2024-04-03 14:37:19 +0900

    Feat: 캠페인 도메인 개발

---
+++ src/main/java/com/ilovegogi/VoiceFinder/domain/campaign/dto/CampaignAdditionalRequestDto.java
@@ -0,0 +1,13 @@
+package com.ilovegogi.VoiceFinder.domain.campaign.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CampaignAdditionalRequestDto {
+
+
+}
